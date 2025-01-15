7cfc34c1a5c4a0367315ae61f53c68701370a02e
Biswajit Ghosal <biswajit.ghosal@exavalu.com>
2025-01-15 18:13:06 +0530

    README.md file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM python:3.9-slim
+FROM python:3.10-slim
 WORKDIR /app
 COPY . /app
 RUN pip install -r requirements.txt
-EXPOSE 3000
+EXPOSE 5000
 CMD ["python", "./app.py"]
--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('',methods=['GET'])
+@app.route('/',methods=['GET'])
 def index():
     return jsonify({
         "message":"You're Connected Successfully."
